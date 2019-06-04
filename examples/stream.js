2ab5f167d901570b5de9488e98f33f02a602482a
Miraclx <omiraculous@gmail.com>
2019-06-04 16:58:33 +0100

    Added simple stream example model

---
+++ examples/stream.js
@@ -0,0 +1,21 @@
+const stream = require('stream');
+const ProgressBar = require('..');
+
+const waterMark = 2 ** 16 - 1;
+const count = 10e3;
+
+const BarGen = ProgressBar.stream(waterMark * count).on('complete', bar => bar.end(`Bar ended\n`));
+
+const r = new stream.Readable({
+  read() {
+    (this.count = (this.count || 0) + 1) <= count ? this.push(Buffer.alloc(waterMark).fill('\x1b[')) : this.push(null);
+  },
+});
+
+const w = new stream.Writable({
+  write(v, e, c) {
+    c(null);
+  },
+});
+
+r.pipe(BarGen.next()).pipe(w);
